7216163e4b85fa9b568a42eb7cd4a4a258b6471d
Jhony Arana <jharanza1986@gmail.com>
2024-01-23 15:08:48 -0500

    feat: Module that take 2 strings and returns one string

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Module that takes 2 strings, concatenate it and returns a string """
+
+
+def concat(str1: str, str2: str) -> str:
+    ''' Method that take 2 strings and concatenate it '''
+    return str1 + str2

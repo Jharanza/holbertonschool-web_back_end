e487c456bc2913ab16a35a180a1145607324b44d
Jhony Arana <jharanza1986@gmail.com>
2024-01-23 16:35:46 -0500

    feat: Sum of a list and returns a float

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Module that returns the sum of a list """
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    ''' Method that sums the values from a list '''
+    sum: float = 0.0
+    for x in input_list:
+        sum += x
+    return sum

d62c7376787fbcc087c663c3e1e67526d487daf0
Jhony Arana <jharanza1986@gmail.com>
2024-01-24 17:41:29 -0500

    fix: change type of annotation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """ Module that create a coroutine that iterate 10 times """
-from typing import AsyncIterator
+from typing import Generator
 import random
 import asyncio
 
 
-async def async_generator() -> AsyncIterator[float]:
+async def async_generator() -> Generator[float, None, None]:
     ''' Generator that yield different random numbers 10 times '''
     for i in range(10):
         await asyncio.sleep(1)

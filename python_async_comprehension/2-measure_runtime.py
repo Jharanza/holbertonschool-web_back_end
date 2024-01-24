7b2599ba64c7d0888d2be55fd28c659094d6610d
Jhony Arana <jharanza1986@gmail.com>
2024-01-24 18:53:03 -0500

    feat: Measure the time it takes four generators to complete in parallel

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Module that take the time that four async tasks take """
+import time
+import asyncio
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    ''' Method that take the time to run an async task '''
+    time_gather = asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    initial: float = time.time()
+    await time_gather
+    final: float = time.time()
+    return final - initial

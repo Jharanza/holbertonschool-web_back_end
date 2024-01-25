3c7f57761982bf4fafd69d0b96bc110e35330e98
Jhony Arana <jharanza1986@gmail.com>
2024-01-25 15:14:15 -0500

    fix: change annotation of return

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -6,7 +6,7 @@ import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> float:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """ Method that run the asynchronous function """
     task: float = asyncio.Task(wait_random(max_delay))
     return task

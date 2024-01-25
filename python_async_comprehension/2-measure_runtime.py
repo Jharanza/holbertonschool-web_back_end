c96f7b1e5f61a4fa8dfc72733e43f5e855b6d69d
Jhony Arana <jharanza1986@gmail.com>
2024-01-25 11:08:25 -0500

    feat: transform variable a list comprehension

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Module that take the time that four async tasks take """
 import time
-from asyncio import gather
+import asyncio
 
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
@@ -9,12 +9,8 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     ''' Method that take the time to run an async task '''
-    time_gather = gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    time_gather = asyncio.gather(*(async_comprehension()
+                                   for _ in range(4)))
 
     initial: float = time.time()
     await time_gather

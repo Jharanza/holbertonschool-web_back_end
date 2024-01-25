1645339cd7debbc7d63fff10f87b9c5f944c94b6
Jhony Arana <jharanza1986@gmail.com>
2024-01-24 22:38:50 -0500

    fix: change import of gather

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Module that take the time that four async tasks take """
 import time
-import asyncio
+from asyncio import gather
 
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
@@ -9,12 +9,13 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     ''' Method that take the time to run an async task '''
-    time_gather = asyncio.gather(
+    time_gather = gather(
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
         async_comprehension()
     )
+
     initial: float = time.time()
     await time_gather
     final: float = time.time()

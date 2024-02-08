ac2d1829bcfec36aa3fa23dc4e839615aaafe2e4
jhony arana <jharanza1986@gmail.com>
2024-02-07 20:44:35 -0500

    feat: clone a car as instance of the parent class

---
+++ ES6_classes/100-evcar.js
@@ -0,0 +1,18 @@
+/**
+ * class that can clone an instance of the class. the cloned instance will be
+ * an instance of the parent class instead of this class
+ */
+
+import Car from './10-car';
+
+export default class EVCar extends Car {
+    constructor(brand, motor, color, range) {
+        super(brand, motor, color);
+        this._range = range;
+    }
+
+    static get [Symbol.species]() {
+        return Car;
+    }
+
+}

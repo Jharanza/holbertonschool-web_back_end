ace1532e7a3ebedec4150a4e0fc0fbbec02c468b
jhony arana <jharanza1986@gmail.com>
2024-02-19 16:25:05 -0500

    fix: delete the args without use

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,11 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then((value) => {
-      if (!value) return false;
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
       return { status: 200, body: 'success' };
     })
-    .then(console.log('Got a response from the API'))
-    .catch(new Error());
-
-  return promise;
+    .catch(() => Error());
 }

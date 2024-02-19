30cd1c0e72788370758436e73f3bdd9eece3b6ff
jhony arana <jharanza1986@gmail.com>
2024-02-19 12:38:17 -0500

    feat: Return a promise with its response

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,16 @@
+/**
+ * Function that returns a promise with a response
+ * @myPromise variable that get the new promise
+ * @export the function
+ * @param {any} success
+ * @returns the result of the promise
+ */
+
+export default function getFullResponseFromAPI(success) {
+  const myPromise = new Promise((resolve, reject) => {
+    if (success) resolve({ status: 200, body: 'Succes' });
+    reject(Error('The fake API is not working currently'));
+  });
+
+  return myPromise;
+}

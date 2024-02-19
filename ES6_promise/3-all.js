daef0d70391e338491c403148ca716d02da09d7c
jhony arana <jharanza1986@gmail.com>
2024-02-19 17:43:38 -0500

    fix: Add try catch

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,12 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
-  const array = [uploadPhoto(), createUser()];
-  const result = Promise.all(array);
+export default async function handleProfileSignup() {
+  try {
+    // Execute both promises concurrently and await their results
+    const [photoData, userData] = await Promise.all([uploadPhoto(), createUser()]);
 
-  result
-    .then((data) => console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`))
-    .catch(() => console.error('Signup system offline'));
-
-  return result;
+    // Extract relevant data and log to console
+    console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
+  } catch (error) {
+    // Log error message if any of the promises rejects
+    console.log('Signup system offline');
+  }
 }

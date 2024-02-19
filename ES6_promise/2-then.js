6f9645fbf5caa7feda96adbb1c8b0b9f7570813e
jhony arana <jharanza1986@gmail.com>
2024-02-19 15:03:23 -0500

    feat: Get the response from a promise

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,11 @@
+export default function handleResponseFromAPI(promise) {
+  promise
+    .then((value) => {
+      if (!value) return false;
+      return { status: 200, body: 'success' };
+    })
+    .then(console.log('Got a response from the API'))
+    .catch(new Error());
+
+  return promise;
+}

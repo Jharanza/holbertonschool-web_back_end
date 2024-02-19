49c46100432368612f1f27766a481693dca711cf
jhony arana <jharanza1986@gmail.com>
2024-02-19 13:11:39 -0500

    fix: fixing typo

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -8,7 +8,7 @@
 
 export default function getFullResponseFromAPI(success) {
   const myPromise = new Promise((resolve, reject) => {
-    if (success) resolve({ status: 200, body: 'Succes' });
+    if (success) resolve({ status: 200, body: 'Success' });
     reject(Error('The fake API is not working currently'));
   });
 

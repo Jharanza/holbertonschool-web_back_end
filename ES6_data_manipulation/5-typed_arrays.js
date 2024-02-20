303e0c078a5f16728bc9b6d1a63a3901da226542
jhony arana <jharanza1986@gmail.com>
2024-02-20 12:13:32 -0500

    feat: Add condition in case position is outside of range

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -2,7 +2,7 @@
  * Function that returns a new ArrayBuffer with an Int8 value at a specific position
  * @arraybuffer an instance of the ArrayBuffer()
  * @view an instance of DataView()
- * @export
+ * @export createInt8TypedArray
  * @param {number} length
  * @param {number} position
  * @param {number} value
@@ -10,9 +10,11 @@
  */
 
 export default function createInt8TypedArray(length, position, value) {
-  if (value < -128 && value > 127) throw Error('Position outside range');
   const arraybuffer = new ArrayBuffer(length);
   const view = new DataView(arraybuffer);
+
+  if (position < 0 || position >= length) throw new Error('Position outside range');
+
   view.setInt8(position, value);
 
   return view;
